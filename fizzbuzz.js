78a0ace31eecc7eeccc8713f2651b361608b117f
kuptsova18 <kuptsova.98@list.ru>
2020-11-14 19:49:13 +0400

    added

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+var n;
+function fizzbuzz(number)
+{  
+    while(number++<n)
+    {
+        if (numder%3==0)
+        System.out.println("fizz")
+        else
+        if (numder%5==0)
+        System.out.println("buzz") 
+        else
+        System.out.println("fizzbuzz")
+    }
+}
